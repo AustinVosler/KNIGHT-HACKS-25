c5db73f75ee174e895a253768796c1e873dd8bca
AlphauroraE <brieoche99@gmail.com>
2025-10-25 02:38:39 -0400

    Initialize Flask Backend

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "This is your Flask backend!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
