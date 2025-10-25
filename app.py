3c70aaa5b0ae2789bd027c433a75f7ee16f5dd69
alexls05 <aleysoto2005@gmail.com>
2025-10-25 03:14:21 -0400

    Added sqlite3 - Browser opens on run

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
+import webbrowser
+from threading import Thread
 from flask import Flask, jsonify, request
+import sqlite3
+
+# Connect to SQLite database (or create it)
+conn = sqlite3.connect('videos.db')
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE IF NOT EXISTS videos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        url TEXT NOT NULL
+    )
+''')
+conn.commit()
 
 app = Flask(__name__)
 
@@ -10,5 +25,9 @@ def home():
 def get_data():
     return jsonify({"message": "This is your Flask backend!"})
 
+def open_browser():
+    webbrowser.open("http://127.0.0.1:5000")
+
 if __name__ == '__main__':
+    Thread(target=open_browser).start()
     app.run(debug=True)
